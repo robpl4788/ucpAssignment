00d4d86360b1d895b75db6d65107c192c2573a69
robpl4788 <robpl@peter.com.au>
2023-09-03 21:32:22 +0800

    cars move but broken not sure why

--- moves.c
+++ moves.c
@@ -2,6 +2,7 @@
 
 #include "moves.h"
 #include "terminal.h"
+#include "constants.h"
 
 void getMove(int rows, int columns, int playerMove[2], int playerPosition[2])
 {
@@ -26,5 +27,30 @@ void getMove(int rows, int columns, int playerMove[2], int playerPosition[2])
     else if (input == 'd' && playerPosition[0] != columns)
     {
         playerMove[0] = 1;
+    }
+}
+
+void moveCars(int* carPositions, int* carDirections, int roadCount, int columns, int playerPosition[2], int* PgameStatus)
+{
+    int i;
+    for (i = 0; i < roadCount; i ++)
+    {
+        int row = i * 2 + 1;
+
+        carPositions[i] = carPositions[i] + carDirections[i];
+
+        if (carPositions[i] == 0)
+        {
+            carDirections[i] == RIGHT;
+        }
+        else if (carPositions[i] == columns - 1)
+        {
+            carDirections[i] == LEFT;
+        }
+
+        if (carPositions[i] == playerPosition[0] && row = playerPosition[1])
+        {
+            *PgameStatus = LOSE;
+        }
     }
 }
